3833815ef020df46bf2c48e1cbd77d6b3b78d9c2
Jethro Rainford <rainford1995@gmail.com>
2022-08-16 17:28:35 +0100

    rename script

--- src/eggd_vcf_rescue.sh
+++ src/eggd_vcf_rescue.sh
@@ -3,7 +3,7 @@
 set -exo pipefail
 
 _validate_inputs() {
-    : '
+    : '''
     Check given input files to ensure correct ones passed for non pass
     recuing or rescuing filtered variants
 
@@ -15,26 +15,26 @@ _validate_inputs() {
             - non_pass_rescue and rescue_filtered booleans both specified
             - required files for non_pass_rescue mode not provided
             - required files for resuce_filtered mode not provided
-    '
+    '''
     mark-section "Validating inputs"
 
-    if [[ "$non_pass_rescue" != "True" && "$rescue_filtered" != "True" ]] || \
-       [[ "$non_pass_rescue" == "True" && "$rescue_filtered" == "True" ]]; then
-        dx-jobutil-report-error "Error: one of --non-pass-rescue or --rescue-filtered \
-            are required and are not mutually exclusive. Please check inputs and re-run \
+    if [[ ("$non_pass_rescue" != "True" && "$rescue_filtered" != "True") \
+        || ("$non_pass_rescue" == "True" && "$rescue_filtered" == "True") ]]; then
+        dx-jobutil-report-error "Error: one of --non-pass-rescue or --rescue-filtered
+            are required and are not mutually exclusive. Please check inputs and re-run
             with only one specified."
         exit 1
     fi
 
-    if [[ "$non_pass_rescue" == "True" ]] && \
-       [[ -z $fasta_tar_name || -z $gvcf || -z $rescue_vcf ]]; then
+    if [[ "$non_pass_rescue" == "True" \
+       && ( -z $fasta_tar_name || -z $gvcf || -z $rescue_vcf ) ]]; then
         dx-jobutil-report-error "Error: non_pass_rescue specified but not all required files
             of fasta_tar, gvcf and rescue_vcf passed"
         exit 1
     fi
 
-    if [[ "$rescue_filtered" ]] && \
-       [[ -z $filtered_vcf || -z $unfiltered_vcf || -z $rescue_vcf ]]; then
+    if [[ "$rescue_filtered" \
+       && ( -z $filtered_vcf || -z $unfiltered_vcf || -z $rescue_vcf ) ]]; then
         dx-jobutil-report-error "Error: rescue_filtered specified but not all required files
             of filtered_vcf, unfiltered_vcf and rescue_vcf passed"
         exit 1
@@ -43,24 +43,31 @@ _validate_inputs() {
 
 
 _rescue_non_pass() {
-    : '
+    : '''
     Rescues non-pass variants from gvcf against given vcf of variants to retain, uploads
     VCF of PASS + rescued variants and exits app.
 
+
     Globals:
         gvcf: given gVCF to rescue non-PASS variants from
-        filter_tag: tag to apply to rescued variants for output VCF
+        filter_tag: tag to apply to FILTER field of rescued variants
 
     Arguments:
         None
 
     Outputs:
         None
-    '
-
-    # Get sample prefix
+    '''
     local sample_prefix
     sample_prefix=$(cut -d'_' -f1 <<< "$gvcf_name")
+
+    # ensure all VCFs are bgzipped and indexed
+    _compress_and_index "$gvcf_name"
+    _compress_and_index "$rescue_vcf_name"
+
+    # set names of compressed VCFs incase they now differ to app input
+    gvcf_name="${gvcf_name/.gz/}.gz"
+    rescue_vcf_name="${rescue_vcf_name/.gz/}.gz"
 
     # Remove reference calls from gvcf
     bcftools view -m2 "$gvcf_name" -o "${sample_prefix}.vcf"
@@ -75,11 +82,10 @@ _rescue_non_pass() {
     # Create a vcf of all NON-PASS which match the rescue sites
     bcftools filter -i 'FILTER!="PASS"' "${sample_prefix}_norm.vcf"   \
         | bcftools filter -m + -s "$filter_tag" --mask-file "${rescue_vcf_name}" - \
-        | bcftools filter -i 'FILTER~"$filter_tag"' - -Oz -o "${sample_prefix}.rescued.vcf.gz"
+        | bcftools filter -i "FILTER~\"${filter_tag}\"" - -Oz -o "${sample_prefix}.rescued.vcf.gz"
 
     # Create a vcf with only PASS variants
-    bcftools view -f PASS "${sample_prefix}_norm.vcf"  -Oz \
-        -o "${sample_prefix}_pass.vcf.gz"
+    bcftools view -f PASS "${sample_prefix}_norm.vcf"  -Oz -o "${sample_prefix}_pass.vcf.gz"
 
     # index vcf files
     bcftools index "${sample_prefix}_pass.vcf.gz" "${sample_prefix}.rescued.vcf.gz"
@@ -89,25 +95,31 @@ _rescue_non_pass() {
         -Oz -o "${sample_prefix}_withLowSupportHotspots.vcf.gz"
 
     # Upload output vcf
-    filtered_vcf=$(dx upload "${sample_prefix}_withLowSupportHotspots.vcf.gz" --brief)
-    dx-jobutil-add-output filtered_vcf "$filtered_vcf" --class=file
-
-    echo "Done"
-    mark-success
+    output_vcf=$(dx upload "${sample_prefix}_withLowSupportHotspots.vcf.gz" --brief)
+    dx-jobutil-add-output output_vcf "$output_vcf" --class=file
 }
 
 
 _rescue_filtered() {
-    : '
+    : '''
     Rescues filtered out variants from a given unfiltered - filtered vcf pair
     against given VCF of variants to retain, uploads rescued VCF and exits app.
+
+    Globals:
+        filtered_vcf : vcf of filtered variants to append to for output vcf
+        unfiltered_vcf : vcf unfiltered variants used to rescue from
+        rescue_vcf : vcf of known variants to rescue against
+        filter_tag : tag to apply to FILTER field of rescued variants
 
     Arguments:
         None
 
     Outputs:
         None
-    '
+    '''
+    local sample_prefix
+    sample_prefix=$(cut -d'_' -f1 <<< "$unfiltered_vcf")
+
     # ensure all VCFs are bgzipped and indexed
     _compress_and_index "$filtered_vcf_name"
     _compress_and_index "$unfiltered_vcf_name"
@@ -118,18 +130,30 @@ _rescue_filtered() {
     unfiltered_vcf_name="${filtered_vcf_name/.gz/}.gz"
     rescue_vcf_name="${rescue_vcf_name/.gz/}.gz"
 
-    mkdir isec_output
+    # remove chr prefix from sample vcf
+    awk '{gsub(/chr/,""); print}' "$unfiltered_vcf_name" > "${sample_prefix}_noChr.vcf"
 
-    bcftools isec "$unfiltered_vcf_name" "$rescue_vcf_name" -p isec_output/
+    # normalise and left align filtered vcf
+    bcftools norm -m -any -f genome.fa "${sample_prefix}_noChr.vcf" \
+        -o "${sample_prefix}_norm.vcf"
 
-    # 0002.vcf has variants in unfiltered vcf that are also present in rescue vcf
-    # tag these as rescued, then concatenate these back to filtered vcf
+    # rescue variants against given rescue vcf
+    bcftools filter -m + -s "$filter_tag" --mask-file "$rescue_vcf_name" "${sample_prefix}_norm.vcf" \
+        | bcftools filter -i "FILTER~${filter_tag}" - -Oz -o "${sample_prefix}.rescued.vcf.gz"
+
+    # combine unfiltered vcf with rescued variants to output
+    outname=$(sed 's/.vcf\|.gz//g' <<< "$filtered_vcf_name").vcf.gz
+    bcftools concat -a -d all "$filtered_vcf_name" "${sample_prefix}.rescued.vcf.gz" -Oz -o "$outname"
+
+    # Upload output vcf
+    output_vcf=$(dx upload "$outname" --brief)
+    dx-jobutil-add-output output_vcf "$output_vcf" --class=file
 
 }
 
 
 _compress_and_index() {
-    : '
+    : '''
     Decompresses given vcf if already compressed, then compresses with
     bgzip and indexes with bcftools index
 
@@ -138,7 +162,7 @@ _compress_and_index() {
 
     Outputs:
         - compressed vcf + index
-    '
+    '''
     input_vcf="$1"
 
     if [[ "$input_vcf" == *.gz ]]; then
@@ -185,4 +209,7 @@ main() {
         # rescuing variants from a filtered VCF and unfiltered vcf
         _rescue_filtered
     fi
+
+    echo "Done!"
+    mark-success
 }
